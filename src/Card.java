65ab52e164a37473a6e9bd5cb40b246f9fcfeb23
kaoreill <kaoreill@ucm.es>
2025-02-05 12:59:10 +0000

    Deck and Card Classes
    Created the Deck and Card classes to allow for a standard 52 card deck

---
+++ src/Card.java
@@ -0,0 +1,14 @@
+class Card {
+    private final String suit;
+    private final String rank;
+
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
