dd784dc6a92872b90ccb0552847da382b56e350f
kaoreill <kaoreill@ucm.es>
2025-02-06 14:01:19 +0000

    Update Deck.java

--- src/Deck.java
+++ src/Deck.java
@@ -1,3 +1,19 @@
-public class Deck {
+import java.util.*;
 
+class Deck {
+    private final List<Card> cards;
+
+    public Deck() {
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+
+        cards = new ArrayList<>();
+
+        for (String suit : suits) {
+            for (String rank : ranks) {
+                cards.add(new Card(rank, suit));
+            }
+        }
+        Collections.shuffle(cards);
+    }
 }
