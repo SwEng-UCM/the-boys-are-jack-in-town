759c4d5cbbabf326a062e2eb43bb6e64500c6e15
David Jimenez Calderon <davidjimenezcalderon@Davids-MacBook-Air.local>
2025-03-11 14:42:04 +0100

    Changes Spansih texts

--- main/view/Texts.java
+++ main/view/Texts.java
@@ -19,7 +19,7 @@ public class Texts {
                     "Kijárat", "مخرج"};
 
     public static String[] options =
-            {"Options", "Opciónes", "Rogha",
+            {"Options", "Opciones", "Rogha",
                     "opciók", "خيارات"};
 
     public static String[] guiTitle =
@@ -31,7 +31,7 @@ public class Texts {
                             "visz", "يأخذ"};
 
     public static String[] guiStand =
-            {"Stand","Queda", " folamh le",
+            {"Stand","Plantar", " folamh le",
                     "\n" +
                             "allvány", "وقوف"};
 
@@ -40,11 +40,11 @@ public class Texts {
                             "új játék", "لعبة جديدة"};
 
     public static String[] guiDealerScore =
-            {"Dealer's Score ","El scor del distribudor", "scor le déanta",
+            {"Dealer's Score ","Puntuación del crupier", "scor le déanta",
                     "Kereskedő pontszáma", "نقاط التاجر"};
 
     public static String[] guiPlayerScore =
-            {"Player's Score ","El scor del jugador", "scor le imreoir",
+            {"Player's Score ","Puntuación del jugador", "scor le imreoir",
                     "Játékos pontszáma", "نقاط اللاعب"};
 
     public static String[] guiWelcome =
@@ -57,26 +57,26 @@ public class Texts {
 
     public static String[] gameManagerGameOn  =
             {"Game On! Your turn.",
-                    "¡Juego encendido! Tu turno.", "Cluiche Ar! Do sheal.",
+                    "¡Empieza el juego! Tu turno.", "Cluiche Ar! Do sheal.",
                             "Game On! Ön a sor.", "اللعبة على! دورك."};
 
     public static String[] dealerBlackjack =
-            {"Dealer has Blackjack! Dealer wins!", "¡El repartidor tiene Blackjack! ¡El repartidor gana!", "Tá Blackjack ag an dáileoir! Buaileann an dáileoir!", "A kereskedőnek Blackjackje van! A kereskedő nyer!", "الموزع لديه بلاك جاك! الموزع يفوز!"};
+            {"Dealer has Blackjack! Dealer wins!", "¡El crupier tiene Blackjack! ¡El repartidor gana!", "Tá Blackjack ag an dáileoir! Buaileann an dáileoir!", "A kereskedőnek Blackjackje van! A kereskedő nyer!", "الموزع لديه بلاك جاك! الموزع يفوز!"};
 
     public static String[] playerBlackjack =
             {"Player has Blackjack! Player wins!", "¡El jugador tiene Blackjack! ¡El jugador gana!", "Tá Blackjack ag an imreoir! Buaileann an t-imreoir!", "A játékosnak Blackjackje van! A játékos nyer!", "اللاعب لديه بلاك جاك! اللاعب يفوز!"};
 
     public static String[] playerBusts =
-            {"Player busts! Dealer wins!", "¡El jugador se pasa! ¡El repartidor gana!", "Tá an t-imreoir imithe thar fóir! Buaileann an dáileoir!", "A játékos túllépte! A kereskedő nyer!", "انفجر اللاعب! الموزع يفوز!"};
+            {"Player busts! Dealer wins!", "¡El jugador se pasa! ¡El crupier gana!", "Tá an t-imreoir imithe thar fóir! Buaileann an dáileoir!", "A játékos túllépte! A kereskedő nyer!", "انفجر اللاعب! الموزع يفوز!"};
 
     public static String[] dealerBusts =
-            {"Dealer busts! Player wins!", "¡El repartidor se pasa! ¡El jugador gana!", "Tá an dáileoir imithe thar fóir! Buaileann an t-imreoir!", "A kereskedő túllépte! A játékos nyer!", "انفجر الموزع! اللاعب يفوز!"};
+            {"Dealer busts! Player wins!", "¡El crupier se pasa! ¡El jugador gana!", "Tá an dáileoir imithe thar fóir! Buaileann an t-imreoir!", "A kereskedő túllépte! A játékos nyer!", "انفجر الموزع! اللاعب يفوز!"};
 
     public static String[] playerWins =
             {"Player wins!", "¡El jugador gana!", "Buaileann an t-imreoir!", "A játékos nyer!", "اللاعب يفوز!"};
 
     public static String[] dealerWins =
-            {"Dealer wins!", "¡El repartidor gana!", "Buaileann an dáileoir!", "A kereskedő nyer!", "الموزع يفوز!"};
+            {"Dealer wins!", "¡El crupier gana!", "Buaileann an dáileoir!", "A kereskedő nyer!", "الموزع يفوز!"};
 
     public static String[] tie =
             {"It's a tie!", "¡Es un empate!", "Tá sé cothrom!", "Döntetlen!", "إنها تعادل!"};
