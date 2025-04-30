180932af3e329b9e3abba9c06a4ae786e6b03a81
EmreEryillmaz <femreeryilmaz@gmail.com>
2025-05-01 00:39:39 +0300

    small

--- main/view/CardImageFactory.java
+++ main/view/CardImageFactory.java
@@ -4,8 +4,10 @@ import main.model.Card;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class CardImageFactory {
+
     public static JPanel createCardPanel(Card card, int width, int height) {
         JPanel cardPanel = new JPanel(new BorderLayout());
         cardPanel.setPreferredSize(new Dimension(width, height));
@@ -13,40 +15,67 @@ public class CardImageFactory {
         cardPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
 
         JLabel imageLabel;
+
         if (card.getType() != Card.CardType.STANDARD) {
-            imageLabel = new JLabel(getSpecialCardIcon(card, width, height));
+            imageLabel = new JLabel(loadSpecialCardImage(card, width, height));
         } else {
-            imageLabel = new JLabel(getStandardCardIcon(card, width, height));
+            imageLabel = new JLabel(loadStandardCardImage(card, width, height));
         }
 
         imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
         imageLabel.setVerticalAlignment(SwingConstants.CENTER);
-
         cardPanel.add(imageLabel, BorderLayout.CENTER);
+
         return cardPanel;
     }
 
-    private static ImageIcon getStandardCardIcon(Card card, int width, int height) {
+    private static ImageIcon loadStandardCardImage(Card card, int width, int height) {
         String rank = card.getRank().toLowerCase();
         String suit = card.getSuit().toLowerCase();
-        String path = "resources/img/cards/" + rank + "_of_" + suit + ".png";
+        String path = "/resources/img/cards/" + rank + "_of_" + suit + ".png";
 
-        ImageIcon icon = new ImageIcon(path);
-        Image scaled = icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
-        return new ImageIcon(scaled);
+        java.net.URL imageURL = CardImageFactory.class.getResource(path);
+        if (imageURL == null) {
+            System.err.println("Missing image: " + path);
+            return createFallbackIcon(width, height, rank + " of " + suit);
+        }
+
+        return scaleIcon(new ImageIcon(imageURL), width, height);
     }
 
-    private static ImageIcon getSpecialCardIcon(Card card, int width, int height) {
+    private static ImageIcon loadSpecialCardImage(Card card, int width, int height) {
         String filename = switch (card.getType()) {
             case BLACKJACK_BOMB -> "blackjack_bomb.png";
             case SPLIT_ACE -> "split_ace.png";
             case JOKER_WILD -> "joker_wild.png";
             default -> "unknown.png";
         };
-        String path = "resources/img/cards/" + filename;
+        String path = "/resources/img/cards/" + filename;
 
-        ImageIcon icon = new ImageIcon(path);
+        java.net.URL imageURL = CardImageFactory.class.getResource(path);
+        if (imageURL == null) {
+            System.err.println("Missing special image: " + path);
+            return createFallbackIcon(width, height, card.toString());
+        }
+
+        return scaleIcon(new ImageIcon(imageURL), width, height);
+    }
+
+    private static ImageIcon scaleIcon(ImageIcon icon, int width, int height) {
         Image scaled = icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
         return new ImageIcon(scaled);
+    }
+
+    private static ImageIcon createFallbackIcon(int width, int height, String labelText) {
+        BufferedImage placeholder = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = placeholder.createGraphics();
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(0, 0, width, height);
+        g.setColor(Color.RED);
+        g.setFont(new Font("Arial", Font.BOLD, 14));
+        g.drawString("Missing", 10, height / 2 - 10);
+        g.drawString(labelText, 10, height / 2 + 10);
+        g.dispose();
+        return new ImageIcon(placeholder);
     }
 }
