79afac820ad98235ae19b1785e6936b303ab96c4
fifarrel <fifarrel@tcd.ie>
2025-05-08 11:42:31 +0200

    Final project UML

--- main/controller/GameManager.java
+++ main/controller/GameManager.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.SwingUtilities;
+import main.controller.GameAction;
 
 /**
  * The GameManager class is responsible for managing the game state and logic.
